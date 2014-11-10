79a31b910bf284404d8997696371eacffa862cc2
KemCake <santos.remi@icloud.com>
2014-11-10 18:28:53 +0100

    Initial Commit

---
+++ SwiftSampleAnimation/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SwiftSampleAnimation
+//
+//  Created by Remi Santos on 10/11/14.
+//  Copyright (c) 2014 Remi Santos. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

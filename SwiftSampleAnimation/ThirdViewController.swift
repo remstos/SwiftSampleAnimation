2c0b326d44d47d084414ab9d2d679cacf3e6dc6f
edouard chusseau <edouard.chusseau@icloud.com>
2014-11-10 18:45:59 +0100

    Add thirdViewController.swift

---
+++ SwiftSampleAnimation/ThirdViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ThirdViewController.swift
+//  SwiftSampleAnimation
+//
+//  Created by EDOUARD CHUSSEAU on 10/11/2014.
+//  Copyright (c) 2014 Remi Santos. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}

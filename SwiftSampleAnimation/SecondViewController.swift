b98ddf30fe51181fba069f8b96d49f3365a136ca
edouard chusseau <edouard.chusseau@icloud.com>
2014-11-10 21:06:04 +0100

    Add Vroom Gesture

--- SwiftSampleAnimation/SecondViewController.swift
+++ SwiftSampleAnimation/SecondViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet var roundGesture: UIImageView!
+    
+    @IBOutlet var Vroum: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        Vroum.hidden = true
+        roundGesture.layer.cornerRadius = roundGesture.frame.size.width/2
     }
 
+    @IBAction func GestureRound(sender: UIPanGestureRecognizer) {
+        let center = sender.locationInView(self.view)
+        roundGesture.center.x = center.x
+        Vroum.hidden = false
+        if(sender.state == UIGestureRecognizerState.Ended){
+            Vroum.hidden = true
+        }
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.

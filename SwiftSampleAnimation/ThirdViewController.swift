28f2c9bdea3acde96240dbc933a0b522194147ce
KemCake <santos.remi@icloud.com>
2014-11-12 00:25:01 +0100

    Yahoo, that's really fun to play with UIDynamics ! Here a gravity and collision example

--- SwiftSampleAnimation/ThirdViewController.swift
+++ SwiftSampleAnimation/ThirdViewController.swift
@@ -10,14 +10,68 @@ import UIKit
 
 class ThirdViewController: UIViewController {
 
+    @IBOutlet weak var ballOne: UIView!
+    var ballOneCenter:CGPoint!
+    @IBOutlet weak var ballTwo: UIView!
+    @IBOutlet weak var ballThree: UIView!
+    
+    var animateUp = false
+    var animator:UIDynamicAnimator!
+    var gravity = UIGravityBehavior()
+    var collision = UICollisionBehavior()
+    var elasticity = UIDynamicItemBehavior()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.tabBarController?.view.backgroundColor = UIColor.whiteColor()
+        animator = UIDynamicAnimator(referenceView: self.view)
+        gravity.gravityDirection = CGVectorMake(0.1, 1)
+        gravity.magnitude = 10
+        animateUp = true
+        animator.addBehavior(gravity)
+        collision.translatesReferenceBoundsIntoBoundary = true
+        animator.addBehavior(collision)
+
+        elasticity.elasticity = 0.4
+        animator.addBehavior(elasticity)
+        
+        ballOne.layer.cornerRadius = ballOne.frame.size.width / 2
+        ballTwo.layer.cornerRadius = ballTwo.frame.size.width / 2
+        ballThree.layer.cornerRadius = ballThree.frame.size.width / 2
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        ballOneCenter = ballOne.center
+        
+        gravity.addItem(ballOne)
+        collision.addItem(ballOne)
+        elasticity.addItem(ballOne)
+        
+        gravity.addItem(ballTwo)
+        collision.addItem(ballTwo)
+        elasticity.addItem(ballTwo)
+        
+        gravity.addItem(ballThree)
+        collision.addItem(ballThree)
+        elasticity.addItem(ballThree)
+    }
+    @IBAction func letsFall(sender: AnyObject) {
+        
+        if (animateUp) {
+            self.upAnimation()
+        } else {
+            self.downAnimation()
+        }
     }
     
+    func upAnimation() {
+        gravity.magnitude = -10
+        animateUp = false
+    }
+    func downAnimation() {
+        gravity.magnitude = 10
+        animateUp = true
+    }
 }

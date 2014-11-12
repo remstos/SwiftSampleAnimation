cf36585afdada8a9662bc26fdd3423d4a1d0bddc
KemCake <santos.remi@icloud.com>
2014-11-12 01:42:00 +0100

    Oops, animating corner radius with block animation doesn't work, better use CABasicAnimation

--- SwiftSampleAnimation/FirstViewController.swift
+++ SwiftSampleAnimation/FirstViewController.swift
@@ -10,11 +10,9 @@ import UIKit
 class FirstViewController: UIViewController {
 
     @IBOutlet var goButton: UIButton!
-    
     @IBOutlet var round1: UIView!
-    
     @IBOutlet var round2: UIView!
-    var round1Center:CGPoint!
+
     var shouldReset:Bool=false
     let swagColor = UIColor(red:0.000000, green:0.776471, blue:0.674510, alpha:1.0)
     
@@ -29,19 +27,13 @@ class FirstViewController: UIViewController {
         round1.backgroundColor = swagColor
         round1.layer.cornerRadius = round1.frame.size.width/2
         //round2 initialize
-        round2.layer.cornerRadius = round2.frame.size.width/2
+        round2.layer.cornerRadius = 0
         round2.backgroundColor = swagColor
         
     }
     
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     @IBAction func goActionButton(sender: AnyObject) {
-        
         
         if shouldReset == true {
             startReset()
@@ -56,7 +48,7 @@ class FirstViewController: UIViewController {
     {
         self.round1.transform = CGAffineTransformIdentity
         round2.backgroundColor = swagColor
-        round2.layer.cornerRadius = round2.frame.size.width/2
+        round2.layer.cornerRadius = 0
         shouldReset = false
         goButton.setTitle("Animate !", forState: .Normal)
     }
@@ -64,16 +56,24 @@ class FirstViewController: UIViewController {
     
     func startAnimation()
     {
-        round1Center = round1.center
-        
+        //Animation with block
         UIView.animateWithDuration(1, animations: { () -> Void in
             //Move round1 to the right
             self.round1.transform = CGAffineTransformMakeTranslation(self.view.frame.size.width-100, 0)
             //Change round2 form and color
             self.round2.backgroundColor = UIColor.blueColor()
-            self.round2.layer.cornerRadius = 0
-            
         })
+        
+        //Core Animation
+        let newCornerRaidus = round2.frame.size.width/2
+        var cornerRadiusAnimation = CABasicAnimation(keyPath:"cornerRadius")
+        cornerRadiusAnimation.fromValue = 0
+        cornerRadiusAnimation.toValue = newCornerRaidus
+        cornerRadiusAnimation.duration = 1.0;
+        round2.layer.addAnimation(cornerRadiusAnimation, forKey: "aChosenKey")
+        round2.layer.cornerRadius = newCornerRaidus
+        
+        
         shouldReset = true
         goButton.setTitle("Reset", forState: .Normal)
     }
